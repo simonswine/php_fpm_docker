a258bcbe5badf328d12c47eb92da05b823613d43
Christian Simon <simon@swine.de>
2014-11-27 22:39:03 +0100

    Fix problems with logging modules
    * wrong namespace

--- lib/php_fpm_docker/logging.rb
+++ lib/php_fpm_docker/logging.rb
@@ -1,14 +1,15 @@
+require 'logger'
+
 module PhpFpmDocker
   # Logging method for all classes
   module Logging
     def logger
       return @logger if @logger
-      @logger = Logger.new 'test'
-      outputter = Outputter.stdout
-      outputter.formatter = PatternFormatter.new(
-        pattern: "%l - #{self.class} - %m"
-      )
-      @logger.outputters = outputter
+      @logger = ::Logger.new STDOUT
+      original_formatter = Logger::Formatter.new
+      @logger.formatter = proc { |severity, datetime, progname, msg|
+          original_formatter.call(severity, datetime, progname, msg.dump)
+      }
       @logger
     end
   end
